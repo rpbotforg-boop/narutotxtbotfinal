013e96cbebdf9a9bc3bebef12a80e66c9a0117e2
adi960280-cmd <adityamalhotra960@gmail.com>
2025-11-11 19:56:17 +0530

    Update config.py

--- Extractor-Final-Naruto-Repo/config.py
+++ Extractor-Final-Naruto-Repo/config.py
@@ -2,28 +2,28 @@ import os
 
 class Config(object):
     # Telegram Bot ka token
-    BOT_TOKEN = "8235729400:AAHL0CaiT9xin7TNmILhFf2ZTmu-VOalvkY"
+    BOT_TOKEN = "7662019980:AAGQnhhOhWxi69j3aWTFfOCUNQhbgtxSz9M"
     # Telegram API ki ID
-    API_ID = 27030443
-    API_HASH = "da842bsdfijfhbeuf39edf0ac74b6e00c43c91"
-    ADMIN_ID = [7991495621]
+    API_ID = 29115102
+    API_HASH = "1a331db2b00e9d2decaa9c7276449eb6"
+    ADMIN_ID = [1224092270]
     # MongoDB database ka URL
     DB_URL = "mongodb+srv://akash1:cluster8343@cluster0.eupasvd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # Database ka naam
     DB_NAME = "MY_BOT_DB"
     # Text log channel ki ID
-    TXT_LOG = -10012234502065
+    TXT_LOG = -1002067304488
     # Authentication log channel ki ID
-    AUTH_LOG = -1002429764735
+    AUTH_LOG = -1002067304488
     # Hit log channel ki ID
     HIT_LOG = -1002262405901
     # DRM dump channel ki ID
     DRM_DUMP = -1002349629748
     # Main channel ki ID
-    CHANNEL = -1002361507681
+    CHANNEL = -1002067304488
     # Channel ka link
-    CH_URL = "https://t.me/+qEEho66dZJ1kODRi"
+    CH_URL = "https://t.me/alfgkhkdd"
     # Bot ke owner ka Telegram link
-    OWNER = "https://t.me/username"
+    OWNER = "https://t.me/@Aditya00923"
     # Thumbnail image ka URL
     THUMB_URL = "https://telegra.ph/file/example-thumb-image.jpg" #Replace by with your Thumb URL
