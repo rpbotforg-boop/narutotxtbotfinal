76348553254657b4a585791a6a68327cb001a0e6
Apnimaa <patelkinjal1212@gmail.com>
2025-12-29 10:07:08 +0530

    Add files via upload

---
+++ Extractor-Final-Naruto-Repo/config.py
@@ -0,0 +1,29 @@
+import os
+
+class Config(object):
+    # Telegram Bot ka token
+    BOT_TOKEN = "8457218709:AAGZpkE5j2YYE0FzoIXxe7d97qSTXcWwVyY"
+    # Telegram API ki ID
+    API_ID = 27433400
+    API_HASH = "1a286620de5ffe0a7d9b57e604293555"
+    ADMIN_ID = [6201066540]
+    # MongoDB database ka URL
+    DB_URL = "mongodb+srv://niravpatel180503_db_user:vjWNaWhRk0gMSNyQ@cluster0.26bfgmf.mongodb.net/?appName=Cluster0"
+    # Database ka naam
+    DB_NAME = "niravpatel180503_db_user"
+    # Text log channel ki ID
+    TXT_LOG = -1003635671191
+    # Authentication log channel ki ID
+    AUTH_LOG = -1003595781984
+    # Hit log channel ki ID
+    HIT_LOG = -1003641539262
+    # DRM dump channel 
+    DRM_DUMP = -1003517811023
+    # Main channel ki ID
+    CHANNEL = -1003510568146
+    # Channel ka link
+    CH_URL = "https://t.me/newextractorapna"
+    # Bot ke owner ka Telegram link
+    OWNER = "https://t.me/Priyansh230819"
+    # Thumbnail image ka URL
+    THUMB_URL = "https://telegra.ph/file/example-thumb-image.jpg" #Replace by with your Thumb URL
