80a47e95189f124d5681cb64e2af91b73bf46c51
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-02 16:15:18 +0530

    Update config.py

--- Extractor-Final-Naruto-Repo/config.py
+++ Extractor-Final-Naruto-Repo/config.py
@@ -2,28 +2,28 @@ import os
 
 class Config(object):
     # Telegram Bot ka token
-    BOT_TOKEN = "8457218709:AAGZpkE5j2YYE0FzoIXxe7d97qSTXcWwVyY"
+    BOT_TOKEN = "8225118430:AAEOFTRcVH3Au1LR0iFyasUb4U5CGATuoT4"
     # Telegram API ki ID
-    API_ID = 27433400
-    API_HASH = "1a286620de5ffe0a7d9b57e604293555"
-    ADMIN_ID = [6201066540]
+    API_ID = 22447622
+    API_HASH = "543b62d58d3e723e766ba57a984ab65d"
+    ADMIN_ID = [777756062]
     # MongoDB database ka URL
     DB_URL = "mongodb+srv://niravpatel180503_db_user:vjWNaWhRk0gMSNyQ@cluster0.26bfgmf.mongodb.net/?appName=Cluster0"
     # Database ka naam
     DB_NAME = "niravpatel180503_db_user"
     # Text log channel ki ID
-    TXT_LOG = -1003635671191
+    TXT_LOG = -1002655703820
     # Authentication log channel ki ID
-    AUTH_LOG = -1003595781984
+    AUTH_LOG = -1003577180094
     # Hit log channel ki ID
-    HIT_LOG = -1003641539262
+    HIT_LOG = -1003577180094
     # DRM dump channel 
-    DRM_DUMP = -1003517811023
+    DRM_DUMP = -1003348766009
     # Main channel ki ID
-    CHANNEL = -1003510568146
+    CHANNEL = -1003566055053
     # Channel ka link
-    CH_URL = "https://t.me/newextractorapna"
+    CH_URL = "https://t.me/+WNYGnEVYHRwzMDll"
     # Bot ke owner ka Telegram link
-    OWNER = "https://t.me/Priyansh230819"
+    OWNER = "https://t.me/@king11up"
     # Thumbnail image ka URL
     THUMB_URL = "https://telegra.ph/file/example-thumb-image.jpg" #Replace by with your Thumb URL
